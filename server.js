baac6c5bf94fbd44f942b79635161b66798fb7d1
Dániel Juhász <juhaszjd@192.168.0.200>
2023-09-16 10:56:36 +0200

    add mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express()
 
 
@@ -7,8 +8,20 @@ app.get('/', (req,res) =>{
     res.send("hello node api");
 });
 
-
-
-app.listen(3000, ()=> {
-    console.log('Node is running');
+app.get('/blog', (req,res) =>{
+    res.send("hello node api blog");
 });
+
+
+
+
+
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.w60u71u.mongodb.net/nodetutorial?retryWrites=true&w=majority').then(() => {
+    console.log("Connected to mongodb")
+    app.listen(3000, ()=> {
+        console.log('Node is running on port 3000');
+    });
+}).catch((error) => {
+    console.log(error)
+})
